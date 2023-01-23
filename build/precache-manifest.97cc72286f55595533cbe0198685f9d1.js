34fc03ade342b83ba71717dd59463473cb87d606
Nilanjit Chakraborty <87519278+nilchakraborty@users.noreply.github.com>
2023-01-23 22:11:22 +0530

    Add files via upload

---
+++ build/precache-manifest.97cc72286f55595533cbe0198685f9d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f58afe6f67f2492e924cb9f73a6d253",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bd4a73d5eb1f44b3b3b",
+    "url": "/static/css/main.6984b989.chunk.css"
+  },
+  {
+    "revision": "cf5bc850794f3700d497",
+    "url": "/static/js/2.a4ce4b84.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a4ce4b84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bd4a73d5eb1f44b3b3b",
+    "url": "/static/js/main.d548e9a6.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
